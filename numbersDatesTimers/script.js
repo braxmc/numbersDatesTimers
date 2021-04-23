19e6601ad27a203cdec09f476fb01a33c8339efe
Braxton McClellan <thebraxmc@gmail.com>
2021-04-22 22:27:23 -0600

    Math and Rounding

--- numbersDatesTimers/script.js
+++ numbersDatesTimers/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value)
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -252,37 +252,81 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
-console.log(23 === 23.0);
+// Lecture 1
 
-console.log(0.1 + 0.2);
-console.log(0.1 + 0.2 === 0.3);
+// Converting and checking numbers
 
-// converting to number
-console.log(Number('23'));
-console.log(+ '23');
+// console.log(23 === 23.0);
 
-// Parsing
-// JS looks for number, string must start with num
-console.log(Number.parseInt('30px', 10));
-console.log(Number.parseInt('e23', 10));
+// console.log(0.1 + 0.2);
+// console.log(0.1 + 0.2 === 0.3);
 
-console.log(Number.parseFloat(' 2.5rem'));
-console.log(Number.parseInt('2.5rem'));
+// // converting to number
+// console.log(Number('23'));
+// console.log(+ '23');
 
-// checking if value is not a number
-console.log('is not a num');
-console.log(Number.isNaN(20));
-console.log(Number.isNaN('20'));
-console.log(Number.isNaN(+'20X'));
-console.log(Number.isNaN(23 / 0));
+// // Parsing
+// // JS looks for number, string must start with num
+// console.log(Number.parseInt('30px', 10));
+// console.log(Number.parseInt('e23', 10));
 
-// checking if value is a number
-console.log('is a num');
-console.log(Number.isFinite(20));
-console.log(Number.isFinite('20'));
-console.log(Number.isFinite(+'20'));
-console.log(Number.isFinite(23 / 0));
+// console.log(Number.parseFloat(' 2.5rem'));
+// console.log(Number.parseInt('2.5rem'));
 
-console.log(Number.isInteger(20));
-console.log(Number.isInteger('20'));
-console.log(Number.isInteger(20 / 0));
+// // checking if value is not a number
+// console.log('is not a num');
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(+'20X'));
+// console.log(Number.isNaN(23 / 0));
+
+// // checking if value is a number
+// console.log('is a num');
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(+'20'));
+// console.log(Number.isFinite(23 / 0));
+
+// console.log(Number.isInteger(20));
+// console.log(Number.isInteger('20'));
+// console.log(Number.isInteger(20 / 0));
+
+/////////////////////////////////////////////////
+
+// Lecture 1
+
+// Math and Rounding
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 18, 23, 11, 2)); // pulls max value
+console.log(Math.max(12, 20, '40', 10, 1));
+console.log(Math.max('48px', 12, 18, 20, 30));
+
+console.log(Math.min(40, 45, 50, 1, 3)); // pulls min value
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) => Math.trunc(Math.random() * (max - min) + 1) + min;
+console.log(randomInt(1, 20));
+
+
+// Rounding Integers
+console.log(Math.trunc(23.44)); // removes decimals
+console.log(Math.round(23.54)); // rounds to nearst whole number
+console.log(Math.ceil(23.44)); // rounds up
+console.log(Math.floor(23.8)); // rounds down
+
+console.log(Math.trunc(-20.5));
+console.log(Math.floor(-24.5)); // on neg it takes down lower
+console.log(Math.ceil(-24.5)); // on neg it puts number higher
+
+// Rounding decimals
+console.log((2.7).toFixed(0)); // converts num to string
+console.log((2.7).toFixed(4)); // nums tell how many nums after .
+console.log((2.754).toFixed(2)); // will cut off decimals also
+console.log(+(2.754).toFixed(2)); // + converts it to a integer again
