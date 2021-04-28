032e47939201b80bdba69797a836a7c37b69c67b
Braxton McClellan <thebraxmc@gmail.com>
2021-04-28 11:41:54 -0600

    timers setTimeout and setInterval

--- numbersDatesTimers/script.js
+++ numbersDatesTimers/script.js
@@ -275,14 +275,17 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value)
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString())
+    setTimeout(function() {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Update UI
-    updateUI(currentAccount);
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString())
+
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
@@ -527,19 +530,52 @@ btnSort.addEventListener('click', function (e) {
 
 // Internationalizing numbers
 
-const num = 3884764.23;
+// const num = 3884764.23;
 
-const options = {
-  style: 'currency',
-  unit: 'celsius',
-  currency: 'USD', // EUR
-  // useGrouping: false,
-}
+// const options = {
+//   style: 'currency',
+//   unit: 'celsius',
+//   currency: 'USD', // EUR
+//   // useGrouping: false,
+// }
 
-console.log('us: ', new Intl.NumberFormat('en-US', options).format(num));
-console.log('germany: ', new Intl.NumberFormat('de-DE', options).format(num));
-console.log('syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
-console.log(
-  navigator.language,
-  new Intl.NumberFormat(navigator.language, options).format(num)
-);
+// console.log('us: ', new Intl.NumberFormat('en-US', options).format(num));
+// console.log('germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+// console.log('syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
+// console.log(
+//   navigator.language,
+//   new Intl.NumberFormat(navigator.language, options).format(num)
+// );
+
+/////////////////////////////////////////////////
+
+// Lecture 8
+
+// Timers: setTimeout and setInterval 
+
+// setTimeout
+const ingredients = ['olives', 'spinach']
+
+const pizzaTimer = setTimeout((ing1, ing2) => 
+console.log(`Here is your pizza with ${ing1} and ${ing2}`), 
+3000, 
+...ingredients);
+console.log('Waiting.....');
+
+// cancels timer if it includes spinach
+if(ingredients.includes('spinach')) {
+  clearTimeout(pizzaTimer)
+};
+
+// setInterval
+setInterval(function() {
+  const now = new Date();
+
+  const options = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  };
+
+  console.log(new Intl.DateTimeFormat('us-en', options).format(now));
+}, 1000); // every 1 seconds it will run again and print to console
